9a21cd535bbf2cf73cd88fe8abd371de2d6e213c
David <david.turley@live.nl>
2023-08-08 01:40:05 +0200

    Removed unused usings

--- DND Pathfinder/ViewModels/MainViewModel.cs
+++ DND Pathfinder/ViewModels/MainViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DND_Pathfinder.ViewModels
+﻿namespace DND_Pathfinder.ViewModels
 {
 	public partial class MainViewModel: ObservableObject
 	{
