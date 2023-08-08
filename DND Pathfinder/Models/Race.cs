d6f902512e0142a8a9ecdc95c8998b9da6258558
David <david.turley@live.nl>
2023-08-08 05:54:36 +0200

    Added part of Race class

---
+++ DND Pathfinder/Models/Race.cs
@@ -0,0 +1,18 @@
+﻿namespace DND_Pathfinder.Models
+{
+	public partial class Race : ObservableObject
+	{
+		[ObservableProperty]
+		private String _name;
+		[ObservableProperty]
+		private List<(AbilityScore.AbilityScoreType AbilitieType, Int32 adjustment)> _racialAbilityModifiers;
+
+		public Race(string name, List<(AbilityScore.AbilityScoreType AbilitieType, Int32 racialAdjustment)> racialAbilityModifiers = null)
+		{
+			Name = name;
+			RacialAbilityModifiers = 
+				racialAbilityModifiers ?? 
+				new List<(AbilityScore.AbilityScoreType AbilitieType, int adjustment)>();
+		}
+	}
+}
